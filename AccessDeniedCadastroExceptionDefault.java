33850680c54831cf264b86b5120886794d60a97e
raafiinha <107967231+raafiinha@users.noreply.github.com>
2022-06-26 20:57:51 -0300

    Update AccessDeniedCadastroExceptionDefault.java

--- AccessDeniedCadastroExceptionDefault.java
+++ AccessDeniedCadastroExceptionDefault.java
@@ -1,4 +1,4 @@
-package com.formento.cadastro.exception;
+package com.usuario.cadastro.exception;
 
 public class AccessDeniedCadastroExceptionDefault extends CadastroExceptionDefault implements CadastroException {
 
