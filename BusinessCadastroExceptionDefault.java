9113da6d9bb08b86c1a0efbb609dc8aeb3e24598
raafiinha <107967231+raafiinha@users.noreply.github.com>
2022-06-26 20:58:36 -0300

    Create BusinessCadastroExceptionDefault.java

---
+++ BusinessCadastroExceptionDefault.java
@@ -0,0 +1,7 @@
+package com.usuario.cadastro.exception;
+
+public class BusinessCadastroExceptionDefault extends CadastroExceptionDefault implements CadastroException {
+
+    public BusinessCadastroExceptionDefault(String message) {
+        super(message);
+    }
