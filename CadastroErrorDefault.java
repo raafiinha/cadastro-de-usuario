058f1e922e36cf7a289b929e15c3c6d519458cdf
raafiinha <107967231+raafiinha@users.noreply.github.com>
2022-06-26 20:59:34 -0300

    Create CadastroErrorDefault.java

---
+++ CadastroErrorDefault.java
@@ -0,0 +1,14 @@
+package com.formento.cadastro.exception;
+
+public class CadastroErrorDefault implements CadastroError {
+
+    private final String mensagem;
+
+    public CadastroErrorDefault(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    @Override
+    public String getMensagem() {
+        return mensagem;
+    }
