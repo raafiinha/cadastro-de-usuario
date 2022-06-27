6014918fc53d749f050b1a6935725927a249c55f
raafiinha <107967231+raafiinha@users.noreply.github.com>
2022-06-26 21:14:23 -0300

    Create CadastroExceptionDefault.java

---
+++ CadastroExceptionDefault.java
@@ -0,0 +1,11 @@
+package com.usuario.cadastro.exception;
+
+public class CadastroExceptionDefault extends RuntimeException implements CadastroException {
+
+    public CadastroExceptionDefault(String message) {
+        super(message);
+    }
+
+    @Override
+    public CadastroError generateCadastroError() {
+        return new CadastroErrorDefault(getMessage());
